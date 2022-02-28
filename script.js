a91ce4d11fa7e48d3988799ee9c04294466fc4b1
amir-hossain113 <amirhossain.cse@gmail.com>
2022-02-28 10:42:12 -0800

    search result showing

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// getting data from url
 const searchPhone = async () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -7,5 +8,25 @@ const searchPhone = async () => {
     const res = await fetch(url);
     const data = await res.json();
 
-    console.log(data);
+    displaySearchResult(data.data);
+}
+
+// showing data on the website
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add("col");
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+                <button class="btn btn-primary">Details</button>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
