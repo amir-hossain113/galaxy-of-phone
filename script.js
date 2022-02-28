43227b91b5be2ef3efc4db5ad7e6d82e2a18741a
amir-hossain113 <amirhossain.cse@gmail.com>
2022-02-28 13:23:48 -0800

    phone detail loaded

--- script.js
+++ script.js
@@ -17,16 +17,27 @@ const displaySearchResult = phones => {
     phones.forEach(phone => {
         const div = document.createElement('div');
         div.classList.add("col");
+        // if(phones.length <= 20){
         div.innerHTML = `
         <div class="card h-100">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">${phone.brand}</p>
-                <button class="btn btn-primary">Details</button>
+                <button onclick="loadPhoneDetail('${phone.slug}')" class="btn btn-primary">Details</button>
             </div>
         </div>
         `;
         searchResult.appendChild(div);
+        // }
     })
+}
+
+
+const loadPhoneDetail = async id => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data.data.slug);
 }
